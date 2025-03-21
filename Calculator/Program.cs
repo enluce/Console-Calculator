8b677a5550f3b542ca2390df69939d7c27b3872d
enluce <86408122+enluce@users.noreply.github.com>
2025-03-21 16:58:45 +0530

    Add Calculator Library Class and using Trace Listeners for Operation Log File

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
 
 bool endApp = false;
+Calculator calculator = new Calculator();
 // Display title as the C# console calculator app.
 Console.WriteLine("Console Calculator in C#\r");
 Console.WriteLine("------------------------\n");
@@ -54,7 +56,7 @@ while (!endApp)
     {
         try
         {
-            result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+            result = calculator.DoOperation(cleanNum1, cleanNum2, op);
             if (double.IsNaN(result))
             {
                 Console.WriteLine("This operation will result in a mathematical error.\n");
